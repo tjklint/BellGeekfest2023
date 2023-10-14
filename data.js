bc642b2b2e41274430e91df6ca7bc0def6929c36
GriffinBonomo <98484718+GriffinBonomo@users.noreply.github.com>
2023-10-14 10:58:27 -0400

    Added key replacement to email output

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 export const templates = [
     {
         name: "Spotify",
-        message: "Dear Spotify Data Protection Team\
+        message: "Dear Spotify Data Protection Team\n\
         I hope this message finds you well. I am writing to formally request the revocation and deletion of my personal data held by Spotify. I value my privacy and wish to exercise my rights under data protection regulations to have my data removed from your records\.\
-        Below is a list of the specific data categories I would like to have revoked and deleted\:\
-        User Data\n\
-        Street Address Data\n\
-        Usage Data\n\
-        Voice Data\n\
-        Payment and Purchase Data\n\
+        Below is a list of the specific data categories I would like to have revoked and deleted\:\n\n\
+        - User Data\n\
+        - Street Address Data\n\
+        - Usage Data\n\
+        - Voice Data\n\
+        - Payment and Purchase Data\n\n\
         I understand that the proper handling of personal data is of utmost importance, and I trust Spotify to manage this request in accordance with applicable data protection laws, including but not limited to the General Data Protection Regulation (GDPR) CCPA: California Consumer Privacy Act and other relevant regulations\.\
         \n\
         I kindly request that you confirm the receipt of this email and the initiation of the process to revoke and delete my data. Additionally, I would appreciate receiving a formal notification once the process has been completed and my data has been erased from your systems\.\
@@ -19,10 +19,8 @@ export const templates = [
         I trust that Spotify will handle this matter with the utmost professionalism and respect for data privacy. I look forward to your prompt response and the confirmation of my request's successful execution\.\
         \n\
         Thank you for your attention to this matter, and I appreciate your assistance in this regard\.\
-        Sincerely\,\
+        Sincerely\,\n\
         [Full_Name] [Email_Address]",
         keys: ["[Email_Address]", "[Spotify_Account_Id]", "[Full_Name]"]
     }
 ];
-
-console.log(templates);
--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import { templates } from "./data";
+import { templates } from "./data.js";
+
+let emailOutput = document.getElementById("emailOutput");
+
+let message = templates[0].message;
+ 
+templates[0].keys.forEach(key => {
+    message = message.replaceAll(key, ":3 :3 :3");
+});
+
+emailOutput.innerText = message;
 
 console.log(templates);
+
