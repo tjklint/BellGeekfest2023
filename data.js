93b4580ae475d6f9b1eda2cd0ac930c2372ac1ce
GriffinBonomo <98484718+GriffinBonomo@users.noreply.github.com>
2023-10-15 11:15:30 -0400

    Added formatting to email templates

--- data.js
+++ data.js
@@ -22,8 +22,8 @@ export const templates = [
         I kindly request that you confirm the receipt of this email and the initiation of the process to revoke and delete my data. Additionally, I would appreciate receiving a formal notification once the process has been completed and my data has been erased from your systems\.\
         Please ensure that this request is executed promptly and in compliance with the appropriate legal and ethical standards to safeguard my privacy. I also request that any third parties with whom my data may have been shared be notified of my request for data removal\.\n\
         \n\
-        My email address for communication is [Email_Address] and my Spotify account details are as follows: [Spotify_Account_Id] [Spotify_Username]\.\
-        \n\
+        My email address for communication is [Email_Address] and my Spotify account details are as follows: [Spotify_Account_Id] & [Spotify_Username]\.\
+        \n\n\
         I trust that Spotify will handle this matter with the utmost professionalism and respect for data privacy. I look forward to your prompt response and the confirmation of my request's successful execution\.\
         \n\n\
         Thank you for your attention to this matter, and I appreciate your assistance in this regard\.\
@@ -44,12 +44,16 @@ export const templates = [
         Dear Netflix Data Protection Team,\
         \n\
         I trust this message finds you well. I am writing to formally request the deletion of the personal information that Netflix has collected as described in your Privacy Statement. In accordance with applicable data protection regulations, I wish to exercise my right to have my data removed from your records.\
+        \n\n\
+        The categories of personal information I am requesting for deletion are as follows:\
+        \n\n\
+        - Information provided to Netflix, including my name, email address, payment method(s), telephone number, and other identifiers (such as an in-game name).\
         \n\
-        The categories of personal information I am requesting for deletion are as follows:\n\
-        Information provided to Netflix, including my name, email address, payment method(s), telephone number, and other identifiers (such as an in-game name).\n\
-        Information collected automatically, such as my activity on the Netflix service, interactions with emails and texts, device IDs, and information gathered through the use of cookies and web beacons.\
-        Information obtained from Partners, which may include search queries, service activation information, and unique identifiers.\
-        Information from other sources, including data provided by service providers, security service providers, payment service providers, Advertisers, Ad Service Providers, Ad Measurement Companies, online and offline information providers, publicly-available sources, and third-party services for Netflix games.\
+        - Information collected automatically, such as my activity on the Netflix service, interactions with emails and texts, device IDs, and information gathered through the use of cookies and web beacons.\
+        \n\
+        - Information obtained from Partners, which may include search queries, service activation information, and unique identifiers.\
+        \n\
+        - Information from other sources, including data provided by service providers, security service providers, payment service providers, Advertisers, Ad Service Providers, Ad Measurement Companies, online and offline information providers, publicly-available sources, and third-party services for Netflix games.\
         \n\n\
         I request that you delete this information as per the provisions outlined in your Privacy Statement and in accordance with relevant data protection regulations. This request encompasses the complete and irreversible removal of the specified personal information from your records, including any third-party entities with whom you may have shared such data. My Account ID is: [Account_Id]\
         \n\n\
@@ -61,7 +65,7 @@ export const templates = [
         \n\n\
         Thank you for your attention to this matter.\
         \n\n\
-        Sincerely,\
+        Sincerely,\n\
         [Full_Name], [Email_Address]",
         keys: ["[Account_Id]", "[Full_Name]", "[Email_Address]"],
         fieldNames: ["Account Id", "Full Name", "Email Address"],
@@ -86,7 +90,7 @@ export const templates = [
         - Geolocation Information\n\
         - Audio and Visual Information\n\
         - Inferences\n\
-        \n\n\
+        \n\
         I request that you delete this information in compliance with your data collection policy and in accordance with relevant data protection regulations. This request entails the complete and irreversible removal of the specified personal information from your records, including any third-party entities with whom you may have shared such data. My account ID and phone number are as follows: [Account_Id] and [Phone_Number]\
         \n\n\
         Furthermore, I emphasize the importance of adhering to the appropriate legal and ethical standards to safeguard my privacy. As per data protection regulations, you are hereby notified that you have 45 days from the date of this communication to comply with this request.\
@@ -115,8 +119,8 @@ export const templates = [
         I trust this message finds you well. I am writing to formally request the deletion of the personal information that Tesla has collected, as outlined in your data collection policy. In accordance with applicable data protection regulations, I wish to exercise my right to have my data removed from your records.\
         \n\n\
         The categories of personal information I am requesting for deletion are as follows:\
-        \n\
-        Information from or about me, which may include personal identifiers such as my name, contact information, and demographic details.\
+        \n\n\
+        - Information from or about me, which may include personal identifiers such as my name, contact information, and demographic details.\
         \n\
         - Information from or about your Tesla vehicle, encompassing data related to my vehicle's performance, usage, and maintenance.\
         \n\
@@ -154,12 +158,12 @@ export const templates = [
         I trust this message finds you well. I am writing to formally request the deletion of the personal information that Tim Hortons has collected, as outlined in your data collection policy. In accordance with applicable data protection regulations, I wish to exercise my right to have my data removed from your records.\
         \n\n\
         The categories of personal information I am requesting for deletion are as follows:\
-        \n\
+        \n\n\
         - Device Information - Including hardware model, IP address, unique device identifiers, operating system version, and device settings used to access Tim Hortons services.\
         \n\
         - Usage Information - Encompassing details about the services used, time and duration of use, specific portions of the App or website accessed, frequency of usage, clicks on links and materials, email interactions, purchases resulting from advertising, search terms, and other interactions with Tim Hortons content, including information stored in cookies and similar technologies.\
         \n\
-        Location Information - Involving computer IP addresses, general location (e.g., city/town/province/country), and mobile device GPS-based location, which is used for finding local restaurants, providing relevant offers and deals, and delivering location-related information.\
+        - Location Information - Involving computer IP addresses, general location (e.g., city/town/province/country), and mobile device GPS-based location, which is used for finding local restaurants, providing relevant offers and deals, and delivering location-related information.\
         \n\n\
         I request that you delete this information in compliance with your data collection policy and in accordance with relevant data protection regulations. This request entails the complete and irreversible removal of the specified personal information from your records, including any third-party entities with whom you may have shared such data. The phone number linked with my account is as follows: [Phone_Number]\
         \n\n\
